4ce766245c0456641883053914528aba30350021
Şahan <sahancava@marun.edu.tr>
2023-08-16 04:09:05 +0300

    asd

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-import { registerRootComponent } from 'expo';
-// import { firebase } from '@react-native-firebase/app';
+// import { registerRootComponent } from 'expo';
 
+// import App from './App';
+// // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+// // It also ensures that whether you load the app in Expo Go or in a native build,
+// // the environment is set up appropriately
+
+// registerRootComponent(App);
+
+
+import { AppRegistry } from 'react-native';
 import App from './App';
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
+import { expo as appConfig } from './app.json';
 
-// firebase.initializeApp();
-registerRootComponent(App);
+const appName = appConfig.name;
+
+AppRegistry.registerComponent('main', () => App);
